c40e6dfc79f0c79dbf4ccfaaad50b43c2ff5f9f0
fabricio4077 <orejona2501>
2016-07-26 09:40:50 -0500

    First commit

---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,40 @@
+dataSource {
+    pooled = true
+    driverClassName = "org.postgresql.Driver"
+    dialect = org.hibernate.dialect.PostgreSQLDialect
+}
+hibernate {
+    cache.use_second_level_cache = false
+    cache.use_query_cache = false
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
+}
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:postgresql://10.0.0.10:5432/nth_tedein"
+            username = "postgres"
+            password = "postgres"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:postgresql://10.0.0.2:5432/nth_tedein"
+            username = "postgres"
+            password = "postgres"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:postgresql://127.0.0.1:5432/nth_tedein"
+            username = "postgres"
+            password = "postgres"
+        }
+    }
+
+}
+
+
---
+++ test/unit/paginaWeb/PaginaWebControllerSpec.groovy
@@ -0,0 +1,20 @@
+package paginaWeb
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(PaginaWebController)
+class PaginaWebControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
